9880799cee23c40e6c06add0ed2523c4c365b2ed
clement <clement@MacBook-Air-de-clement.local>
2026-03-25 22:37:13 +0100

    ajout Google Analytics GA4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: {
@@ -42,6 +43,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr" suppressHydrationWarning>
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-HFC34SP8SC"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-HFC34SP8SC');
+          `}
+        </Script>
+      </head>
       <body>
         {children}
       </body>
